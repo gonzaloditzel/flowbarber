c6ef112a66f129a411baa46dc1db88d2c8d1e578
gditzel <gonza.ditzel5@gmail.com>
2022-04-29 14:53:32 -0300

    Update

--- js/main.js
+++ js/main.js
@@ -6,3 +6,18 @@ ham.addEventListener('click', () => {
     enlaces.classList.toggle('activado');
     barras.forEach(child => child.classList.toggle('animado'));
 });
+
+let animado = document.querySelectorAll(".animado");
+
+function mostrarScroll() {
+  let scrollTop = document.documentElement.scrollTop;
+  for (var i = 0; i < animado.length; i++) {
+    let alturaAnimado = animado[i].offsetTop;
+    if (alturaAnimado - 700 < scrollTop) {
+      animado[i].style.opacity = 1;
+      animado[i].classList.add("mostrarArriba");
+    }
+  }
+}
+
+window.addEventListener("scroll", mostrarScroll);
